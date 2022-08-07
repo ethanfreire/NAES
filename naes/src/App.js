b3dc07d8fe3323b06b0d1bbfe365c72778ebc05a
ethanfreire <element55star@gmail.com>
2022-08-07 00:50:36 -0400

    setup up sign and install firebase

--- naes/src/App.js
+++ naes/src/App.js
@@ -1,7 +1,7 @@
 import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
-
+import SignIn from "./routes/sign-in/sign-in.component";
 const Shop = () => {
   return(
     <div>
@@ -11,13 +11,14 @@ const Shop = () => {
     </div>
   )
 }
-
+ 
 const App = () => {
   return ( 
     <Routes>
       <Route path='/' element={<Navigation></Navigation>}>
         <Route index element={<Home></Home>}></Route>
         <Route path='/shop' element={<Shop></Shop>}></Route>
+        <Route path='/sign-in' element={<SignIn></SignIn>}></Route>
       </Route>
     </Routes>
   );
