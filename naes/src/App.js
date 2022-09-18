4d72d5521e4cb738cad8d430c836e4895467337a
ethanfreire <element55star@gmail.com>
2022-09-17 23:10:08 -0400

    performance optimization to app js lazy imports, memo cartItem component

--- naes/src/App.js
+++ naes/src/App.js
@@ -1,13 +1,15 @@
-import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
-import NavigationBar from "./routes/navigation-bar/navigation-bar.component";
-import SignIn from "./routes/sign-in/sign-in.component";
-import SignUp from "./routes/sign-up/sign-up.component";
-import Shop from "./routes/shop/shop.component";
-import Checkout from "./routes/checkout/checkout.component";
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { useDispatch } from "react-redux";
-import {  checkUserSession } from "./store/user/user.action";
+import { checkUserSession } from "./store/user/user.action";
+import Spinner from "./components/spinner/spinner.component";
+
+const Home = lazy(() => import("./routes/home/home.component"));
+const NavigationBar = lazy(() =>import("./routes/navigation-bar/navigation-bar.component"));
+const SignIn = lazy(() => import("./routes/sign-in/sign-in.component"));
+const SignUp = lazy(() => import("./routes/sign-up/sign-up.component"));
+const Shop = lazy(() => import("./routes/shop/shop.component"));
+const Checkout = lazy(() => import("./routes/checkout/checkout.component"));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -17,15 +19,17 @@ const App = () => {
   }, []);
 
   return (
-    <Routes>
-      <Route path="/" element={<NavigationBar></NavigationBar>}>
-        <Route index element={<Home></Home>}></Route>
-        <Route path="shop/*" element={<Shop></Shop>}></Route>
-        <Route path="sign-in" element={<SignIn></SignIn>}></Route>
-        <Route path="sign-up" element={<SignUp></SignUp>}></Route>
-        <Route path="checkout" element={<Checkout></Checkout>}></Route>
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner></Spinner>}>
+      <Routes>
+        <Route path="/" element={<NavigationBar></NavigationBar>}>
+          <Route index element={<Home></Home>}></Route>
+          <Route path="shop/*" element={<Shop></Shop>}></Route>
+          <Route path="sign-in" element={<SignIn></SignIn>}></Route>
+          <Route path="sign-up" element={<SignUp></SignUp>}></Route>
+          <Route path="checkout" element={<Checkout></Checkout>}></Route>
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 export default App;
